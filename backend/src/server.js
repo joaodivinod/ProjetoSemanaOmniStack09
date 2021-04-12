43b0b68422f982324a83f2b5a02353bff728095b
João Divino <joaodivinod@gmail.com>
2021-04-12 14:26:55 -0300

    Rotas implementadas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express();
 
-app.listen(3333)
+app.use(express.json());
+app.use(routes);
 
-app.get('/',(req,res) =>{
-    return res.json({message:'Funciona'})
-})
+app.listen(3333)
