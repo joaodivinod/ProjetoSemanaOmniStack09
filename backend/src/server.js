2e082a6b9e1912c80c31ee5c683e8d0199567e16
João Divino <joaodivinod@gmail.com>
2021-05-16 16:43:07 -0300

    WIP

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const app = express();
+
+mongoose.connect('mongodb+srv://joaodivino:30351010@semana9.7elle.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+    userUnifiedTopology:true,
+})
+
+
 
 app.use(express.json());
 app.use(routes);
